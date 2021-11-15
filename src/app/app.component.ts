7ce9013ec1335663fd411b95b69b9dc3ea7d40ee
nandoProgrammer <nandinhop.costa@gmail.com>
2021-11-15 10:18:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: '<p>ok</p>',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-first-project';
+
+  public todos: Todo[] = [];
+  public Title: string = 'Minhas tarefas';
+
+
+  constructor(){
+    this.todos.push(new Todo(1, 'ok', false));
+  }
+
+  alteraTexto(){
+    this.Title = 'Todos'
+  }
+
+  remove(todo: Todo){
+     const index = this.todos.indexOf(todo);
+
+     if(index !== -1){
+       this.todos.splice(index, 1);
+     }
+  }
+
+  markAsDone(todo: Todo){
+
+    todo.done = true;
+
+  }
+
+  markAsUndone(todo: Todo){
+
+    todo.done = false;
+
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,9 @@
+export class Todo{
+   constructor(
+     public id: Number,
+     public todo: String,
+     public done: Boolean,
+   ) {
+
+   }
+}
